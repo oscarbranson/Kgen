617af7e759e77f87028fc9b34d7842713949971e
Oscar Branson <ob266@cam.ac.uk>
2024-07-19 08:59:03 +0100

    start of test framework

--- julia/kgen/test/runtests.jl
+++ julia/kgen/test/runtests.jl
@@ -1,6 +1,26 @@
-using kgen
+using Kgen
 using Test
+using CSV
 
-@testset "kgen.jl" begin
-    # Write your tests here.
+project_path(parts...) = normpath(joinpath(@__DIR__, "..", parts...))
+
+check_Ks = JSON.parsefile(project_path("..", "check_values", "check_Ks.json"))
+
+check_presscorr = JSON.parsefile(project_path("..", "check_values", "check_presscorr.json"))
+
+@testset "K values" begin
+    calced_Ks = Kgen.calc_Ks(TempC=check_Ks["input_conditions"]["TC"], Sal=check_Ks["T"])
+
+    for (k, v) in calced_Ks
+        # assert almost equal calced_k to check_k
+    end
+
+end
+
+@testset "Pressure correction factors" begin
+    pressure_factors = Kgen.prescorr(TC=check_presscorr["input_conditions"]["TC"], P=check_presscorr["input_conditions"]["P"])
+    
+    for (k, f) in pressure_factors
+        # assert f almost equal to reference values
+    end
 end
